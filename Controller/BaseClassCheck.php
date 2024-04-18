d0b4c16069cacbd22443e0602c352d5a50b03d00
sridharan01234 <sridharan01234@gmail.com>
2024-04-18 13:41:40 +0530

    base class with derived classes added for final keyword implementation

---
+++ Controller/BaseClassCheck.php
@@ -0,0 +1,11 @@
+<?php
+require "./BaseClass.php";
+
+class BaseClassCheck extends BaseClass {
+
+    //public const var = "Hiiii";
+    public function __construct() {
+        parent::__construct();
+    }
+}
+?>
--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -1,14 +1,26 @@
 <?php
 
-abstract class BaseController {
+class BaseController
+{
     /**
      * Error logger
-     * 
+     *
      * @param string $log
      * @return void
      */
-     public function logger(string $log):void
-     {
+    public function logger(string $log): void
+    {
         error_log($log);
-     }
+    }
+
+        /**
+     * Error logger
+     *
+     * @param string $log
+     * @return void
+     */
+    final public function log(string $log): void
+    {
+        error_log($log);
+    }
 }
